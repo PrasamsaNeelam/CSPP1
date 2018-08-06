47884c40c839099149c355dee6bf14efcdb8501e
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-06 23:10:34 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -0,0 +1,44 @@
+'''
+Author: Prasamsa
+Date: 6 august 2018
+'''
+
+
+def paying_debt(balance_up, annualinterest_rate, guess_num):
+    '''function for paying debt'''
+    balance_copy = balance_up
+
+    iterator_i = 1
+    while iterator_i <= 12:
+        montly_interest_rate = annualinterest_rate / 12
+        monthly_unpaid_balance = balance_copy - guess_num
+        balance_copy = monthly_unpaid_balance + (montly_interest_rate * monthly_unpaid_balance)
+        iterator_i += 1
+    return balance_copy
+
+def paying_debt_off_in_a_year(balance_up, annualinterest_rate):
+    '''function for paying deft off in a year'''
+    approximation_val = 0.03
+    montly_interest_rate = annualinterest_rate /12.0
+    high_val = (balance_up * (1 + montly_interest_rate) ** 12) / 12.0
+    low_val = balance_up / 12
+    middle_val = (low_val + high_val) / 2.0
+
+    while abs(paying_debt(balance_up, annualinterest_rate, middle_val)) >= approximation_val:
+        if approximation_val <= paying_debt(balance_up, annualinterest_rate, middle_val):
+            low_val = middle_val
+        else:
+            high_val = middle_val
+        middle_val = (low_val + high_val) / 2.0
+    min_pay = middle_val
+    return "Lowest Payment: "+str(round(min_pay, 2))
+
+def main():
+    '''main function'''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print(paying_debt_off_in_a_year(data[0], data[1]))
+
+if __name__ == "__main__":
+    main()
