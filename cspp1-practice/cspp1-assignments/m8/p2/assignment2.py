ff27b92f3196501fc47d5b0b21a3644fca0777e1
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-07 20:57:37 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1-practice/cspp1-assignments/m8/p2/assignment2.py
+++ cspp1-practice/cspp1-assignments/m8/p2/assignment2.py
@@ -3,7 +3,6 @@ Author: Prasamsa
 Date: 7 august 2018
 '''
 
-
 def sum_of_digits(number):
     '''
     n is positive Integer
@@ -13,13 +12,11 @@ def sum_of_digits(number):
     if number == 0:
         return number
     return number%10 + sum_of_digits(number//10)
-    
-
 
 def main():
+	'''to calculate the sum of digits'''
     a_value = input()
-    print(sum_of_digits(int(a_value)))  
+    print(sum_of_digits(int(a_value)))
 
 if __name__== "__main__":
     main()
-
