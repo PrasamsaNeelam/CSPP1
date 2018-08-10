36c80b55258693c214aa1b6e34232fcb87f1a7da
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-10 19:25:16 +0530

    " -> 6 of 6 passed. pylint: 5.62/10 "

--- cspp1-practice/cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-practice/cspp1-assignments/m11/p2/assignment2.py
@@ -26,16 +26,16 @@ def updateHand(hand, word):
 
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
+    '''main function'''
+    n = input()
+    adict = {}
+    for i in range(int(n)):
+        data = input()
+        l = data.split()
+        adict[l[0]] = int(l[1])
+    data1 = input()
+    print(updateHand(adict, data1))
 
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
