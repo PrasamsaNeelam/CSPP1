c27467c6051a6946839f764a4729b717b602c591
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-11 12:53:29 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-practice/cspp1-assignments/m12/p1/create_social_network.py
@@ -35,7 +35,7 @@ def create_social_network(data):
     list_1 = []
     a_dict = {}
     list_1.append(data)
-    for i in len(list_1):
+    for i in range(len(list_1)):
         l2_in = i.split("follows")
         if l2_in[0] in a_dict:
             l2_in[1] = l2_in[1].split(",")
