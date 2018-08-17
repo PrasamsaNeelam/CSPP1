5e08ec2508099e44908c6537b3d0ccb1f98dde90
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-17 10:32:22 +0530

    " -> 0 of 3 passed. pylint: 7.73/10 "

--- cspp1-practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -8,13 +8,14 @@ def similarity(dict1, dict2):
     '''
     lower_case_one = dict1.lower()
     lower_case_two = dict2.lower()
+    for i in lower_case_one:
+        for j in i:
+            if j in (',' or '.' or '?'):
+                del j
+    print(lower_case_one)
     word_list_one = lower_case_one.split(" ")
     word_list_two = lower_case_two.split(" ")
-    for i in word_list_one:
-    	for j in i:
-    		if j in (',' or '.' or '?'):
-    			del j
-    print(word_list_one)
+    
    
 
 
