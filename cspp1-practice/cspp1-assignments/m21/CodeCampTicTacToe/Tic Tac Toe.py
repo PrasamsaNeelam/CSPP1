863b8c4f64720ab7bcc65e48cf17aca90f88d5bc
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-24 14:29:57 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-practice/cspp1-assignments/m21/CodeCampTicTacToe/Tic Tac Toe.py
+++ cspp1-practice/cspp1-assignments/m21/CodeCampTicTacToe/Tic Tac Toe.py
@@ -3,16 +3,16 @@ Author: Prasamsa
 Date: 24 august 2018
 '''
 def is_horizontal(li_st, turn):
-	'''checks for horizontal match'''
-	count = 0
-	for i in range(3):
-		for j in range(3):
-			if not li_st[i][j] is turn:
-			    count += 1
-		if count == 0:
-			   return True
-		count = 0
-	return False
+    '''checks for horizontal match'''
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if not li_st[i][j] is turn:
+                count += 1
+        if count == 0:
+               return True
+        count = 0
+    return False
 
 def is_vertical(li_st, turn):
     '''checks for vertical match'''
@@ -49,43 +49,43 @@ def is_diagnol_backward(li_st, turn):
     return False
 
 def main():
-	'''main function'''
-	count = 0
-	x_count = 0
-	o_count = 0
-	char_count = 0
-	other_count = 0
-	first_row = input().split()
-	second_row = input().split()
-	third_row = input().split()
-	li_st = [first_row, second_row, third_row]
-	for i in range(3):
-	    for j in range(3):
-	        if li_st[i][j] == 'x':
-	        	x_count += 1
-	        elif li_st[i][j] == 'o':
-	        	o_count += 1
-	        elif li_st[i][j] == '.':
-	            char_count += 1
-	        else:
-	        	other_count += 1
-	if other_count != 0:
-		print("invalid input")
-	    count += 1
-	elif x_count > o_count+1 or o_count > x_count+1:
-		print("invalid game")
-		count +=1
-	turn = 'x'
-	boolean_x = (is_horizontal(li_st, turn_x)
+    '''main function'''
+    count = 0
+    x_count = 0
+    o_count = 0
+    char_count = 0
+    other_count = 0
+    first_row = input().split()
+    second_row = input().split()
+    third_row = input().split()
+    li_st = [first_row, second_row, third_row]
+    for i in range(3):
+        for j in range(3):
+            if li_st[i][j] == 'x':
+                x_count += 1
+            elif li_st[i][j] == 'o':
+                o_count += 1
+            elif li_st[i][j] == '.':
+                char_count += 1
+            else:
+                other_count += 1
+    if other_count != 0:
+        print("invalid input")
+        count += 1
+    elif x_count > o_count+1 or o_count > x_count+1:
+        print("invalid game")
+        count +=1
+    turn = 'x'
+    boolean_x = (is_horizontal(li_st, turn_x)
                  or is_vertical(li_st, turn_x)
                  or is_diagnol_forward(li_st, turn_x)
                  or is_diagnol_backward(li_st, turn_x))
-	turn = 'o':
-	boolean_o = (is_horizontal(li_st, turn_o)
+    turn = 'o':
+    boolean_o = (is_horizontal(li_st, turn_o)
                  or is_vertical(li_st, turn_o)
                  or is_diagnol_forward(li_st, turn_o)
                  or is_diagnol_backward(li_st, turn_o))
-	if boolean_x and boolean_o and count == 0:
+    if boolean_x and boolean_o and count == 0:
         print("invalid game")
         count += 1
     if boolean_x and count == 0:
