4613d0ac17ca01307eb808deff471d128754d6e6
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-08-25 11:14:23 +0530

    " -> 0 of 2 passed. pylint: 7.00/10 "

--- cspp1-practice/cspp1-assignments/m22/assignment1/read_input.py
+++ cspp1-practice/cspp1-assignments/m22/assignment1/read_input.py
@@ -5,11 +5,15 @@ Write a python program to read multiple lines of text input and store the input 
 def main():
     '''read input strings'''
     n_val = int(input())
+    l = []
     string_final = ""
     for _ in range(n_val):
         string_input = input()
-        string_final += "".join((string_input).split("\n"))
-    print(string_final)
+        l.append(string_input)
+    string_final = "".join(l)
+        
+    #     string_final += "".join((string_input).split("\n"))
+    # print(string_final)
 
 if __name__ == '__main__':
     main()
